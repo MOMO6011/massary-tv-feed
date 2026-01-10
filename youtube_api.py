98b33b0046ad2520ad92b4d1e863b59c07c6bd72
MOMO6011 <mohamedomar6011@gmail.com>
2026-01-10 13:12:46 +0300

    Update youtube_api.py

--- youtube_api.py
+++ youtube_api.py
@@ -1,30 +1,52 @@
 import json
 import os
 import requests
-from datetime import datetime
 
 API_KEY = os.environ.get("YOUTUBE_API_KEY")
 
-# حط هنا Channel IDs اللي انت عايزها
 CHANNEL_IDS = [
-    "UC_x5XG1OV2P6uZZ5FSM9Ttw"  # مثال (Google Developers)
+    "UCcZAb104e_K7yJc8e_hPyDQ",  # DroosOnline4u
+    "UCSFHcQ6-5uayv5v7yLQFUYA",  # thedocwaleed
+    "UCQqN3qgYbkfd0EkdhJmN5tQ",  # ElzeroWebSchool
+    "UCQqN3qgYbkfd0EkdhJmN5tQ",  # ElzeroTube
+    "UCWg_cj4kueGU2jXJ7Klk5qg",  # Bahaa.Henish
+    "UCscz2NaWRYuaDrwKBJkkVLQ",  # GhareebElshaikh
+    "UCdNo5yauE8IU-vS8_dO3qew",  # abouzaid
+    "UCUdtq-Fvlw7_NjzduNTZsbg",  # Abdullah_yw
+    "UChbuH4HULlesX_rzlozkT6Q",  # AliMuhammadAli
+    "UCaUZb4SGMAFVOJMqXQM3V1w",  # a5drcom
+    "UCMZME066uGTeRUZcDQWaipA",  # WalidTaha
+    "UC_nOoE4cIapZENcimY3I0uQ",  # KonoozTube
+    "UC5RkBPuSzbyudZI0UA1B_Vw",  # TaherART
+    "UC8eFjAmIUnLKLnfJZFPz6QQ",  # Ghanayem
+    "UChmti2i_-Mn4_HE5liAfoXA",  # AhmedShahinOfficial
+    "UCsQqxYEBQHILQS237kcoDKw",  # KareemEsmail
+    "UC_o7uZRKgYgpv2SNhdCNmdg",  # EmadRashadOsman
+    "UCVO0eI6y9QE0IHnpR2rxy5Q",  # EB85_
+    "UC19LSQvi6ca8luKEWL0pdKw",  # W_wakeup
+    "UCQPalfEYxVLs8nEB4LutApQ",  # thmanyahPodcasts
+    "UCxHBGJc2HfCZbv2MgJ6F2Sw",  # BidonWaraq
+    "UC89xhPO7T5uRpPK9Jl7NJrA",  # aramtv
+    "UCfEjNgz1vYDYyury8q7PmYQ"   # MicsPodcas
 ]
 
 FEED_FILE = "feed_1.json"
-MAX_VIDEOS = 50
+MAX_VIDEOS = 50  # أقصى عدد فيديوهات
+feed = []
 
-# قراءة البيانات القديمة
+existing_links = set()
+
+# قراءة بيانات سابقة لو موجودة
 try:
     with open(FEED_FILE, "r", encoding="utf-8") as f:
         feed = json.load(f)
+        existing_links = {item.get("link") for item in feed if "link" in item}
 except:
     feed = []
 
-existing_links = {item.get("link") for item in feed if "link" in item}
-
 for channel_id in CHANNEL_IDS:
     url = (
-        "https://www.googleapis.com/youtube/v3/search"
+        f"https://www.googleapis.com/youtube/v3/search"
         f"?key={API_KEY}"
         f"&channelId={channel_id}"
         "&part=snippet"
@@ -32,17 +54,12 @@ for channel_id in CHANNEL_IDS:
         "&maxResults=5"
         "&type=video"
     )
-
-    response = requests.get(url)
-    data = response.json()
-
+    data = requests.get(url).json()
     for item in data.get("items", []):
         video_id = item["id"]["videoId"]
         link = f"https://www.youtube.com/watch?v={video_id}"
-
         if link in existing_links:
             continue
-
         feed.append({
             "title": item["snippet"]["title"],
             "channel": item["snippet"]["channelTitle"],
@@ -56,7 +73,7 @@ for channel_id in CHANNEL_IDS:
 # ترتيب الأحدث أولًا
 feed = sorted(feed, key=lambda x: x["date"], reverse=True)
 
-# تحديد عدد أقصى للفيديوهات
+# تحديد أقصى عدد فيديوهات
 feed = feed[:MAX_VIDEOS]
 
 # حفظ الملف
